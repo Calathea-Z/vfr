e3b4926158d96b2fd3ce148c5925f1917e53a60a
Zach Sykes <zsykes21@gmail.com>
2024-06-09 10:19:06 -0400

    Logo header motion is a 95% good place. Good for now!

--- components/Nav/Header.tsx
+++ components/Nav/Header.tsx
@@ -1,8 +1,9 @@
 "use client";
 import { simpleLogo } from "@/public/assets";
 //--Framework--//
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { useRouter } from "next/navigation";
+import { usePathname } from "next/navigation"; // Import usePathname
 import Image from "next/image";
 import Link from "next/link";
 //---Packages---//
@@ -11,11 +12,36 @@ import { Microscope, UserCircle, Basket, List } from "@phosphor-icons/react";
 import { motion } from "framer-motion";
 
 const Header = () => {
-	const navigate = useRouter();
+	const router = useRouter();
+	const pathname = usePathname();
+	const [isFirstLoad, setIsFirstLoad] = useState(true);
 	const [isMenuOpen, setIsMenuOpen] = useState(false);
 
+	useEffect(() => {
+		// Check sessionStorage for the first visit flag
+		const firstVisit = sessionStorage.getItem("firstVisit");
+
+		if (firstVisit) {
+			setIsFirstLoad(false);
+		} else {
+			if (pathname === "/") {
+				sessionStorage.setItem("firstVisit", "false");
+				setIsFirstLoad(true);
+			}
+		}
+
+		// Set isFirstLoad to false after the animation duration only if it's the first load
+		if (isFirstLoad && pathname === "/") {
+			const timer = setTimeout(() => {
+				setIsFirstLoad(false);
+			}, 4000);
+
+			return () => clearTimeout(timer);
+		}
+	}, [pathname, isFirstLoad]);
+
 	const handleNavigate = (url: string) => {
-		navigate.push(url);
+		router.push(url);
 	};
 
 	return (
@@ -28,25 +54,33 @@ const Header = () => {
 					<Link href="/info/stockists">Stockists</Link>
 					<Link href="/info">About</Link>
 				</div>
-				<motion.div
-					className="flex-1 justify-center hidden md:flex rounded-lg"
-					initial={{ opacity: 0, backgroundColor: "#F5F5F5" }}
-					animate={{
-						opacity: [0, 1, 1, 1],
-						backgroundColor: ["#F5F5F5", "#f2c88c", "#f2c88c", "#F5F5F5"],
-						scale: [0.5, 0.7, 0.9, 1.0],
-					}}
-					exit={{ opacity: 1, backgroundColor: "#F5F5F5", scale: 1.0, y: 0 }}
-					transition={{
-						duration: 2,
-						ease: "easeInOut",
-						times: [0, 0.2, 0.8, 1],
-					}}
-				>
-					<Link href="/">
-						<Image src={simpleLogo} alt="Logo" width={80} height={80} />
-					</Link>
-				</motion.div>
+				{pathname === "/" && isFirstLoad ? (
+					<motion.div
+						className="flex-1 justify-center hidden md:flex rounded-lg"
+						initial={{ opacity: 0, backgroundColor: "#F5F5F5" }}
+						animate={{
+							opacity: [0, 1, 1, 1],
+							backgroundColor: ["#F5F5F5", "#f2c88c", "#f2c88c", "#F5F5F5"],
+							scale: [0.5, 0.85, 0.9, 0.95, 1.0], // Added more scale values for an even smoother grow and shrink effect
+						}}
+						exit={{ opacity: 1, backgroundColor: "#F5F5F5", scale: 1.0, y: 0 }}
+						transition={{
+							duration: 4,
+							ease: "easeInOut",
+							// times: [0, 0.3, 0.7, 0.9], // Adjusted timing for smoother transitions
+						}}
+					>
+						<Link href="/">
+							<Image src={simpleLogo} alt="Logo" width={80} height={80} />
+						</Link>
+					</motion.div>
+				) : (
+					<div className="flex-1 justify-center hidden md:flex rounded-lg">
+						<Link href="/">
+							<Image src={simpleLogo} alt="Logo" width={80} height={80} />
+						</Link>
+					</div>
+				)}
 				<div className="hidden md:flex md:flex-1 justify-end space-x-4">
 					<IconButton onClick={() => handleNavigate("/search")}>
 						<Microscope size={24} />

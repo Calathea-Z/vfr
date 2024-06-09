0a400a3b0c5b012a9b71a16107182a7ab3d40efb
Zach Sykes <zsykes21@gmail.com>
2024-06-09 06:58:21 -0400

    Trying to fix decrepency between css in local and deployed site.

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import "./globals.css";
 import type { Metadata } from "next";
 
 //---Fonts---//
-import { playfairDisplay } from './fonts/fonts';
+import { playfairDisplay } from "./fonts/fonts";
 
 export const metadata: Metadata = {
 	title: "Vine & Frond",
@@ -10,16 +10,18 @@ export const metadata: Metadata = {
 };
 
 export default function RootLayout({
-    children,
+	children,
 }: {
-    children: React.ReactNode;
+	children: React.ReactNode;
 }) {
-
-    return (
-        <html lang="en">
-            <body className={playfairDisplay.className}>
-                {children}
-            </body>
-        </html>
-    );
+	return (
+		<html lang="en">
+			<head>
+				<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+				<link rel="icon" type="image/svg+xml" href="/icon.svg" />
+				<link rel="stylesheet" href="./globals.css" />
+			</head>
+			<body className={playfairDisplay.className}>{children}</body>
+		</html>
+	);
 }

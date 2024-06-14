c3241a5bead9dfbb136179f53d1790075f6c204c
Zach Sykes <zsykes21@gmail.com>
2024-06-14 14:44:47 -0400

    Google Maps and Stockist set up and working.

---
+++ app/components/googleMaps/GoogleMapCustomOverlay.jsx
@@ -0,0 +1,97 @@
+import React, { useEffect, useRef, useState } from "react";
+import { createRoot } from "react-dom/client";
+
+// GoogleMapCustomOverlay component definition
+// This component is responsible for creating a custom overlay on a Google Map.
+// It takes in props such as the map instance, google maps object, position for the overlay, children components, and an onClose function.
+
+const GoogleMapCustomOverlay = ({
+	map,
+	google,
+	position,
+	children,
+	onClose,
+}) => {
+	// useRef hooks to keep track of the div element and the React root instance for the overlay content.
+	const divRef = useRef();
+	const rootRef = useRef();
+	// useState hook to manage the unmounting state of the overlay.
+	const [isUnmounting, setIsUnmounting] = useState(false);
+
+	// useEffect hook to handle the lifecycle of the custom overlay.
+	useEffect(() => {
+		// CustomOverlay constructor function, creates a new overlay instance.
+		function CustomOverlay(map, position) {
+			this.position = position; // Position of the overlay on the map.
+			this.div = document.createElement("div"); // Creates a new div element for the overlay content.
+			this.setMap(map); // Sets the map instance for the overlay.
+		}
+
+		// Inheriting from google.maps.OverlayView to integrate with Google Maps API.
+		CustomOverlay.prototype = new google.maps.OverlayView();
+
+		// onAdd method, called when the overlay is added to the map.
+		CustomOverlay.prototype.onAdd = function () {
+			this.div.style.position = "absolute"; // Sets the position style of the div to absolute.
+			divRef.current = this.div; // Assigns the div element to the divRef.
+			this.getPanes().floatPane.appendChild(this.div); // Appends the div to the floatPane of the map.
+			if (!rootRef.current) {
+				rootRef.current = createRoot(this.div); // Creates a React root instance for the div if it doesn't exist.
+			}
+		};
+
+		// draw method, called to update the overlay's position or content.
+		CustomOverlay.prototype.draw = function () {
+			const overlayProjection = this.getProjection(); // Gets the map projection.
+			const sw = overlayProjection.fromLatLngToDivPixel(
+				new google.maps.LatLng(this.position.lat, this.position.lng) // Converts the overlay's LatLng position to pixel position on the map.
+			);
+			const div = divRef.current;
+			div.style.left = `${sw.x - 102}px`; // Positions the div 3 rem to the left of the calculated position.
+			div.style.top = `${sw.y}px`; // Positions the div at the calculated top position.
+			if (rootRef.current && !isUnmounting) {
+				// Renders the children components inside the div if it's not unmounting.
+				rootRef.current.render(
+					<div className="custom-overlay flex flex-col relative">
+						{children}
+						<button
+							className="text-white font-bold rounded-full p-2 bg-[#008080]  h-5 w-5 flex items-center justify-center self-end absolute right-[-.4rem] mt-[-.01rem]"
+							onClick={onClose}
+						>
+							X
+						</button>
+					</div>
+				);
+			}
+		};
+		// onRemove method, called when the overlay is removed from the map.
+		CustomOverlay.prototype.onRemove = function () {
+			setIsUnmounting(true); // Sets the unmounting state to true.
+			if (divRef.current) {
+				if (rootRef.current) {
+					setTimeout(() => {
+						if (rootRef.current) {
+							rootRef.current.unmount(); // Unmounts the React root instance.
+							rootRef.current = null; // Clears the rootRef.
+						}
+						if (divRef.current) {
+							divRef.current.parentNode.removeChild(divRef.current); // Removes the div from its parent node.
+							divRef.current = null; // Clears the divRef.
+						}
+						setIsUnmounting(false); // Resets the unmounting state.
+					}, 0);
+				}
+			}
+		};
+
+		const overlay = new CustomOverlay(map, position); // Creates a new CustomOverlay instance.
+
+		return () => {
+			overlay.setMap(null); // Removes the overlay from the map on cleanup.
+		};
+	}, [map, google, position, children, onClose, isUnmounting]); // Dependencies for the useEffect hook.
+
+	return null; // This component does not render anything itself.
+};
+
+export default GoogleMapCustomOverlay;
--- app/info/stockists/page.tsx
+++ app/info/stockists/page.tsx
@@ -1,9 +1,248 @@
-const Stockists = () => {
+"use client";
+import GoogleMapCustomOverlay from "../../components/googleMaps/GoogleMapCustomOverlay";
+import client from "../../../sanity/lib/client";
+//---Framework---//
+import { useState, useEffect, useRef, useCallback } from "react";
+import Link from "next/link";
+//---Packages---//
+import { GoogleMap, LoadScriptNext, Marker } from "@react-google-maps/api";
+import { motion } from "framer-motion";
+
+interface Stockist {
+	name: string;
+	description: string;
+	addressLineOne: string;
+	addressLineTwo: string;
+	keywords: string[];
+	latitude: number;
+	longitude: number;
+	url: string;
+}
+
+const Stockists: React.FC = () => {
+	const svgIcon = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path fill="#059669" stroke="#ffffff" stroke-width="6" d="M272 96c-78.6 0-145.1 51.5-167.7 122.5c33.6-17 71.5-26.5 111.7-26.5h88c8.8 0 16 7.2 16 16s-7.2 16-16 16H288 216s0 0 0 0c-16.6 0-32.7 1.9-48.3 5.4c-25.9 5.9-49.9 16.4-71.4 30.7c0 0 0 0 0 0C38.3 298.8 0 364.9 0 440v16c0 13.3 10.7 24 24 24s24-10.7 24-24V440c0-48.7 20.7-92.5 53.8-123.2C121.6 392.3 190.3 448 272 448l1 0c132.1-.7 239-130.9 239-291.4c0-42.6-7.5-83.1-21.1-119.6c-2.6-6.9-12.7-6.6-16.2-.1C455.9 72.1 418.7 96 376 96L272 96z"/></svg>`;
+
+	const svgIconUrlEncoded = encodeURIComponent(svgIcon);
+	const dataUrl = `data:image/svg+xml,${svgIconUrlEncoded}`;
+
+	const [stockists, setStockists] = useState<Stockist[]>([]);
+	const [mapCenter, setMapCenter] = useState<{ lat: number; lng: number }>({
+		lat: 35.542613827211696, // Default center of the map (12 Bones, Asheville, NC)
+		lng: -82.51833478527368,
+	});
+
+	const [zoomLevel, setZoomLevel] = useState<number>(11);
+	const [selectedStockist, setSelectedStockist] = useState<Stockist | null>(
+		null
+	);
+	const [error, setError] = useState<string | null>(null);
+	const [isMapsScriptLoaded, setIsMapsScriptLoaded] = useState<boolean>(false);
+	const loadScriptRef = useRef<boolean | null>(null); // useRef to manage LoadScriptNext component
+	const mapRef = useRef<GoogleMap | null>(null); // useRef for GoogleMap instance
+
+	const fetchData = async () => {
+		try {
+			const response = await client.fetch<Stockist[]>(
+				`*[_type == "stockist"]{
+			name,
+			description,
+			"addressLineOne": address.street,
+			"addressLineTwo": address.city + ", " + address.state + " " + address.zipCode,
+			keywords,
+			latitude,
+			longitude,
+			url
+		}`
+			);
+			const sortedResponse = response.sort((a, b) =>
+				a.name.localeCompare(b.name)
+			);
+			setStockists(sortedResponse);
+		} catch (error) {
+			console.error(error);
+			setError("Failed to fetch stockists. Please try again later.");
+		}
+	};
+
+	useEffect(() => {
+		fetchData();
+	}, []);
+
+	useEffect(() => {
+		if (!loadScriptRef.current) {
+			loadScriptRef.current = true;
+		}
+	}, []);
+
+	if (error) {
+		return (
+			<div className="bg-primary flex flex-col min-h-screen">
+				<main className="flex-grow p-8 mt-32 flex justify-center items-center">
+					<h1 className="text-4xl font-bold mb-8 text-red-500">{error}</h1>
+					<button onClick={fetchData} className="mt-4 btn btn-primary">
+						Retry
+					</button>
+				</main>
+			</div>
+		);
+	}
+	const mapContainerStyle = {
+		width: "100%",
+		height: "100%",
+	};
+
+	const handleCardClick = useCallback(
+		(latitude: number, longitude: number, stockist: Stockist) => {
+			setMapCenter({ lat: latitude, lng: longitude });
+			setZoomLevel(19); // Zoom in closer when a card is clicked
+			setSelectedStockist(stockist); // Automatically select the stockist to show its info window
+		},
+		[]
+	);
+	const renderMap = () => (
+		<LoadScriptNext
+			googleMapsApiKey={process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY || ""}
+			onLoad={() => setIsMapsScriptLoaded(true)}
+		>
+			<GoogleMap
+				mapContainerStyle={mapContainerStyle}
+				center={mapCenter}
+				zoom={zoomLevel}
+				options={{
+					fullscreenControl: true,
+					disableDefaultUI: true,
+					gestureHandling: "cooperative",
+					styles: [
+						{
+							featureType: "poi.business",
+							elementType: "labels",
+							stylers: [{ visibility: "off" }],
+						},
+					],
+				}}
+				onLoad={(map: google.maps.Map) => {
+					mapRef.current = map as unknown as GoogleMap; // Assign the map instance to mapRef with type casting
+				}} // Assign the map instance to mapRef
+			>
+				{stockists.map((stockist) => (
+					<Marker
+						key={stockist.name}
+						position={{
+							lat: stockist.latitude,
+							lng: stockist.longitude,
+						}}
+						label={{
+							text: stockist.name,
+							color: "#ffffff",
+							fontSize: "10px",
+							fontWeight: "bold",
+							className: "bg-[#008080] rounded px-1 py-0.5", // Custom class for label styling
+						}}
+						icon={{
+							url: dataUrl, //svgIcon
+							scaledSize: new window.google.maps.Size(35, 35),
+							labelOrigin: new window.google.maps.Point(0, -20), // Position label above the icon
+						}}
+						onClick={() =>
+							handleCardClick(stockist.latitude, stockist.longitude, stockist)
+						}
+					/>
+				))}
+				{selectedStockist && (
+					<GoogleMapCustomOverlay
+						map={mapRef.current} // Use the map instance from mapRef
+						google={window.google}
+						position={{
+							lat: selectedStockist.latitude,
+							lng: selectedStockist.longitude,
+						}}
+						onClose={() => setSelectedStockist(null)}
+					>
+						<div className="bg-[#f5f5f5] p-2 rounded-lg shadow-md text-center">
+							<h2 className="text-[#3a7ca5] font-bold">
+								{selectedStockist.name}
+							</h2>
+							<p className="font-bold text-gray-800">
+								{selectedStockist.description}
+							</p>
+							<p className="italic text-[#333333]">
+								{selectedStockist.addressLineOne}
+							</p>
+							<p className="italic text-[#333333]">
+								{selectedStockist.addressLineTwo}
+							</p>
+							{selectedStockist.url && (
+								<Link
+									href={selectedStockist.url}
+									target="_blank"
+									rel="noopener noreferrer"
+									className="text-[#008080] hover:underline"
+								>
+									Visit Website
+								</Link>
+							)}
+							<Link
+								href={`https://www.google.com/maps/dir/?api=1&destination=${encodeURIComponent(
+									selectedStockist.addressLineOne +
+										" " +
+										selectedStockist.addressLineTwo
+								)}`}
+								target="_blank"
+								rel="noopener noreferrer"
+								className="block mt-2 text-[#008080] hover:underline"
+							>
+								Get Directions
+							</Link>
+						</div>
+					</GoogleMapCustomOverlay>
+				)}
+			</GoogleMap>
+		</LoadScriptNext>
+	);
 	return (
-		<div className="flex min-h-screen flex-col items-center justify-between p-24 bg-primary">
-			<p className="text-secondary">Stockists</p>
+		<div className="bg-primary flex flex-col min-h-screen p-0">
+			<div className="grid grid-cols-1 lg:grid-cols-4 justify-center gap-0 p-0">
+				{" "}
+				{/* Removed gap between columns */}
+				<div className="col-span-2 xl:col-span-1 order-2 lg:order-1 w-full bg-slate-200">
+					{stockists.map((stockist) => (
+						<div
+							key={stockist.name}
+							className="bg-white p-6 border-b-[.1rem] border-b-gray-300  cursor-pointer"
+							onClick={() =>
+								handleCardClick(stockist.latitude, stockist.longitude, stockist)
+							}
+						>
+							<h2 className="text-2xl font-bold mb-4 hover:text-gray-800 hover:underline hover:underline-offset-2">
+								{stockist.url ? (
+									<Link
+										href={stockist.url}
+										target="_blank"
+										rel="noopener noreferrer"
+										className="hover:text-gray-800"
+									>
+										{stockist.name}
+									</Link>
+								) : (
+									<span className="hover:text-gray-800">{stockist.name}</span>
+								)}
+							</h2>
+							<p className="text-sm italic mb-4 text-emerald-600">
+								{stockist.keywords ? stockist.keywords.join(", ") : "N/A"}
+							</p>
+							<p className="mb-4 text-base">{stockist.description}</p>
+							<p className="text-base">{stockist.addressLineOne}</p>
+							<p className="text-base">{stockist.addressLineTwo}</p>
+						</div>
+					))}
+				</div>
+				<div className="col-span-2 xl:col-span-3 order-1 lg:order-2 p-0">
+					<div className="sticky top-0 bg-white lg:rounded-md shadow-sm flex justify-center p-3 h-[500px] md:h-[500px] lg:h-[900px]">
+						{renderMap()}
+					</div>
+				</div>
+			</div>
 		</div>
 	);
 };
-
 export default Stockists;

8f281844ac038a724515206a531c752810253684
Zach Sykes <zsykes21@gmail.com>
2024-06-16 10:48:00 -0400

    Sign out is working properly

--- app/user/logout/page.tsx
+++ app/user/logout/page.tsx
@@ -1,26 +1,19 @@
-import { signOut } from "@/nextAuth";
+"use client";
 
-export default function LogoutPage(): JSX.Element {
+import { useSnackbar } from "notistack";
+import LogoutButton from "../../components/auth/LogOutButton";
+
+export default function LogoutPage() {
+	const { enqueueSnackbar } = useSnackbar();
+
+	const handleLogout = () => {
+		enqueueSnackbar("Successfully logged out", { variant: "success" });
+	};
+
 	return (
-		<div className="flex items-start justify-center h-screen bg-primary">
-			<div className="block max-w-sm p-10 bg-primary border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700 text-center mt-10">
-				<h3 className="mb-2 text-xl font-bold tracking-tight text-gray-900 dark:text-white flex flex-row justify-center items-center gap-2">
-					Are you sure you want to sign out?
-				</h3>
-				<form
-					action={async (formData) => {
-						"use server";
-						await signOut({ redirectTo: "/" });
-					}}
-				>
-					<button
-						type="submit"
-						className="font-lg rounded-md bg-zinc-100 hover:bg-zinc-200 cursor-pointer p-4"
-					>
-						Sign Out
-					</button>
-				</form>
-			</div>
+		<div>
+			<h1>Logout</h1>
+			<LogoutButton onLogout={handleLogout} />
 		</div>
 	);
 }

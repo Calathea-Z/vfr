e1bb19faaaea928eedff795f28981a844f8e24cb
Zach Sykes <zsykes21@gmail.com>
2024-06-15 21:56:50 -0400

    Fix deployment error in error page.

--- app/info/auth-error-page/page.tsx
+++ app/info/auth-error-page/page.tsx
@@ -1,7 +1,8 @@
 "use client";
 
 import { Bug } from "@phosphor-icons/react";
-import { useParams, useSearchParams } from "next/navigation";
+import { useSearchParams } from "next/navigation";
+import { Suspense } from "react";
 
 enum Error {
 	Configuration = "Configuration",
@@ -17,24 +18,32 @@ const errorMap = {
 	),
 };
 
-export default function AuthErrorPage() {
+function ErrorContent() {
 	const search = useSearchParams();
 	const error = search.get("error") as Error;
 
 	return (
+		<a
+			href="/"
+			className="block max-w-sm p-10 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700 text-center"
+		>
+			<h5 className="mb-2 text-xl font-bold tracking-tight text-gray-900 dark:text-white flex flex-row justify-center items-center gap-2">
+				<Bug size={32} />
+				Something went wrong
+			</h5>
+			<div className="font-normal text-gray-700 dark:text-gray-400">
+				{errorMap[error] || "Please contact us if this error persists."}
+			</div>
+		</a>
+	);
+}
+
+export default function AuthErrorPage() {
+	return (
 		<div className="flex flex-col items-center justify-start pt-10 w-full h-screen bg-gradient-to-l from-red-300 to-secondary">
-			<a
-				href="/"
-				className="block max-w-sm p-10 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700 text-center"
-			>
-				<h5 className="mb-2 text-xl font-bold tracking-tight text-gray-900 dark:text-white flex flex-row justify-center items-center gap-2">
-					<Bug size={32} />
-					Something went wrong
-				</h5>
-				<div className="font-normal text-gray-700 dark:text-gray-400">
-					{errorMap[error] || "Please contact us if this error persists."}
-				</div>
-			</a>
+			<Suspense fallback={<div>Loading...</div>}>
+				<ErrorContent />
+			</Suspense>
 			<div></div>
 		</div>
 	);
